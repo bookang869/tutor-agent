66f12a1906c102fc5175dc06727caa46db1aabc6
Kyle <kb0531@cs.washington.edu>
2025-11-25 17:57:02 -0800

    feat: introducedd a router to determine which agent the user will be transferred to

--- main.py
+++ main.py
@@ -8,14 +8,28 @@ from agents.teacher_agent import teacher_agent
 load_dotenv()
 
 class TutorState(MessagesState):
-  pass
+  current_agent: str
+
+# router to determine which agent the user will be transferred to
+def router_check(state: TutorState):
+  current_agent = state.get("current_agent", "classification_agent")
+  return current_agent
 
 graph_builder = StateGraph(TutorState)
 
-graph_builder.add_node("classification_agent", classification_agent, destinations=["teacher_agent", "feynman_agent"])
+graph_builder.add_node("classification_agent", classification_agent, destinations=("teacher_agent", "feynman_agent"))
 graph_builder.add_node("feynman_agent", feynman_agent)
 graph_builder.add_node("teacher_agent", teacher_agent)
 
-graph_builder.add_edge(START, "classification_agent")
+graph_builder.add_conditional_edges(
+  START, 
+  router_check, 
+  [
+    "feynman_agent", 
+    "teacher_agent", 
+    "classification_agent"
+  ]
+)
+graph_builder.add_edge("classification_agent", END)
 
 graph = graph_builder.compile()
